871dc9354b1af9737e621a4ddb3c6083187cdaa0
Dave Marquess <dave.marquess@gmail.com>
2019-04-20 18:49:04 -0700

    Add ConfigGeneration components and basic styling

--- src/client/components/configGeneration/ConfigGeneration.jsx
+++ src/client/components/configGeneration/ConfigGeneration.jsx
@@ -1,6 +1,31 @@
 import React from 'react';
+import { observer, inject } from 'mobx-react';
+import WhiteCardConfigWelcome from './WhiteCardConfigWelcome';
+import WhiteCardConfigSelectRoot from './WhiteCardConfigSelectRoot';
+// import WhiteCardConfigBuildConfig from './WhiteCardConfigBuildConfig';
+import styles from '../../stylesheets/modules/tab3/configGeneration.module';
+
+const initialState = {
+  checkedReact: false,
+  checkedTypescript: false,
+  checkedCSS: false,
+  checkedSass: false,
+  checkedLess: false,
+  checkedStylus: false,
+  checkedSVG: false,
+  checkedPNG: false,
+  rootCustomDirectory: '',
+  defaultFormattedCode: '',
+  AST: {},
+}
+
+@inject('store')
+@observer
 
 class ConfigGeneration extends React.Component {
+  state = initialState;
+  _isMounted = false;
+
   constructor() {
     super();
     this.state = {
@@ -9,8 +34,24 @@ class ConfigGeneration extends React.Component {
   }
 
   render() {
+    const codeString = '(num) => num + 1';
+    const { store } = this.props;
     return (
-      <div className=''>TabThree is working!</div>
+      <div className={styles.mainContainerHome}>
+        <div>
+
+          <WhiteCardConfigWelcome
+            isRootSelected={store.isRootSelected}
+          />
+
+          {!store.isRootSelected &&
+            <WhiteCardConfigSelectRoot
+              // selectCustomWebConfigRoot={this.selectCustomWebConfigRoot}
+            />
+          }
+
+        </div>
+      </div>
     );
   }
 }
