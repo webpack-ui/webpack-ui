db42091014a8fd3e2fadf5afd1d652635ddb07be
Dave Marquess <dave.marquess@gmail.com>
2019-05-26 17:59:23 -0700

    add png-loader, fix navigation container position styling when more loader options displaying

--- src/client/components/DefaultStarter.jsx
+++ src/client/components/DefaultStarter.jsx
@@ -421,6 +421,63 @@ class DefaultStarter extends React.Component {
     });
   }
 
+  handleChangeCheckboxPNG = () => {
+    let { checkedPNG, pngAST, numberOfRules, moduleExist } = this.state;
+    const customAST = JSON.parse(JSON.stringify(this.state.customAST));
+    let formattedCode;
+    const customASTPropertyKey = [];
+    customAST.body[customAST.body.length - 1].expression.right.properties.forEach((el) => {
+      customASTPropertyKey.push(el.key.name)
+    })
+    if (!checkedPNG) {
+      if (customASTPropertyKey.indexOf(pngAST.body[0].expression.right.properties[0].key.name) === -1) {
+        moduleExist = true;
+        numberOfRules += 1;
+        customAST.body[customAST.body.length - 1].expression.right.properties.push(JSON.parse(JSON.stringify(pngAST.body[0].expression.right.properties[0])));
+      } else {
+        customAST.body[customAST.body.length - 1].expression.right.properties.forEach((el) => {
+          if (el.key.name === "module") {
+            let moduleArr = el.value.properties
+            moduleArr.forEach((moduleEl) => {
+              if (moduleEl.key.name === "rules") {
+                moduleEl.value.elements.push(JSON.parse(JSON.stringify(pngAST.body[0].expression.right.properties[0].value.properties[0].value.elements[0])));
+              }
+            })
+          }
+        })
+        moduleExist = true;
+        numberOfRules += 1;
+      }
+    } else {
+      let module_index = 0;
+      for (let i = 0; i < customAST.body[customAST.body.length - 1].expression.right.properties.length; i += 1) {
+        if (customAST.body[customAST.body.length - 1].expression.right.properties[i].key.name === "module") module_index = i
+      }
+
+      if (numberOfRules === 1 && customAST.body[customAST.body.length - 1].expression.right.properties[module_index].value.properties.length === 1) {
+        customAST.body[customAST.body.length - 1].expression.right.properties.splice(module_index, 1)
+        numberOfRules -= 1;
+      } else if (numberOfRules > 1 && customAST.body[customAST.body.length - 1].expression.right.properties[module_index].value.properties.length === 1) {
+        for (let j = 0; j < customAST.body[customAST.body.length - 1].expression.right.properties[module_index].value.properties[0].value.elements.length; j += 1) {
+          if (customAST.body[customAST.body.length - 1].expression.right.properties[module_index].value.properties[0].value.elements[j].properties[0].value.raw.includes(".png")) {
+            customAST.body[customAST.body.length - 1].expression.right.properties[module_index].value.properties[0].value.elements.splice(j, 1)
+            numberOfRules -= 1;
+          }
+        }
+      }
+    }
+    formattedCode = generate(customAST, {
+      comments: true,
+    })
+    this.setState({
+      checkedPNG: !this.state.checkedPNG,
+      numberOfRules,
+      moduleExist,
+      customAST,
+      formattedCode
+    });
+  }
+
   saveWebpackConfig = () => {
     const link = document.createElement('a');
     link.download = 'webpack.config.js';
@@ -486,6 +543,10 @@ class DefaultStarter extends React.Component {
         <label>
           <input type="checkbox" name="framework" value="SVG" checked={this.state.checkedSVG} onChange={this.handleChangeCheckboxSVG} />
           svg-loader
+        </label>
+        <label>
+          <input type="checkbox" name="framework" value="PNG" checked={this.state.checkedPNG} onChange={this.handleChangeCheckboxPNG} />
+          png-loader
         </label>
         <div className={styles.navigationContainer}>
           <button onClick={() => this.setState({ displayLoader: false, displayFramework: true })}>
