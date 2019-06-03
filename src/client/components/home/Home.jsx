c6f8be4c3bff50d48aaadf9fb46715d7c030b51c
Dave Marquess <dave.marquess@gmail.com>
2019-06-03 00:47:36 -0700

    Configure dev server to work with node server thru proxy

--- src/client/components/home/Home.jsx
+++ src/client/components/home/Home.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import { observer, inject } from 'mobx-react';
-import hero from '../../assets/images/hero.png'
+import hero from '../../assets/images/hero.png';
 import styles from '../../stylesheets/modules/home/home.module';
-import { withRouter } from "react-router-dom";
-
+import { withRouter } from 'react-router-dom';
 
 @inject('store')
 @observer
 class Home extends React.Component {
-
   constructor(props) {
     super(props);
-    this.state = {
-    }
+    this.state = {};
   }
 
   render() {
@@ -22,17 +19,21 @@ class Home extends React.Component {
         <div className={styles.header}>Dashboard</div>
         <div className={styles.lowerContainer}>
           <div className={styles.leftContainer}>
-            <img className={styles.hero} src={hero} alt="" />
+            <img className={styles.hero} src={hero} alt='' />
             <div className={styles.header2}>No Project Found</div>
-            <div className={styles.welcomeMessage}>We guess this is your first time! Create a project now to get started!</div>
-            <button onClick={()=>{this.props.history.push("/newProject")}}> 
+            <div className={styles.welcomeMessage}>
+              We guess this is your first time! Create a project now to get started!
+            </div>
+            <button
+              onClick={() => {
+                this.props.history.push('/newProject');
+              }}>
               <span>+</span> Create Project
             </button>
-
           </div>
           <div className={styles.rightContainer}>
-            <div style={{width: "320px"}}>No Activity here. Create a project to get started</div>
-            <hr/>
+            <div style={{ width: '320px' }}>No Activity here. Create a project to get started</div>
+            <hr />
           </div>
         </div>
       </div>
