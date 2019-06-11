f43153b6dc9399e608ed9147f622a7b78cc2146f
Rishabh <rishabh31121999@gmail.com>
2019-06-11 17:05:42 +0530

    chore: rewite recursive parser in js

--- src/utils/recursive-parser.js
+++ src/utils/recursive-parser.js
@@ -1,9 +1,8 @@
 import * as utils from "./ast-utils";
-import { IJSCodeshift, INode, valueType } from "./types/NodePath";
 
 export default function recursiveTransform(
-	j: IJSCodeshift, ast: INode, key: string, value: valueType, action: string,
-): boolean | INode {
+	j, ast, key, value, action,
+) {
 	if (key === "topScope") {
 		if (Array.isArray(value)) {
 			return utils.parseTopScope(j, ast, value, action);
@@ -15,12 +14,12 @@ export default function recursiveTransform(
 			return utils.parseMerge(j, ast, value, action);
 		}
 	}
-	const node: INode = utils.findRootNodesByName(j, ast, key);
+	const node = utils.findRootNodesByName(j, ast, key);
 
 	// get module.exports prop
 	const root = ast
 		.find(j.ObjectExpression)
-		.filter((p: INode): boolean => {
+		.filter((p) => {
 			return (
 				utils.safeTraverse(p, [
 					"parentPath",
@@ -38,19 +37,19 @@ export default function recursiveTransform(
 				]) === "exports"
 			);
 		})
-		.filter((p: INode): boolean => p.value.properties);
+		.filter((p) => p.value.properties);
 
 	if (node.size() !== 0) {
 		if (action === "add") {
 			return utils.findRootNodesByName(j, root, key)
-				.forEach((p: INode) => {
+				.forEach((p) => {
 					j(p).replaceWith(utils.addProperty(j, p, key, value, action));
 				});
 		} else if (action === "remove") {
 			return utils.removeProperty(j, root, key, value);
 		}
 	} else {
-		return root.forEach((p: INode) => {
+		return root.forEach((p) => {
 			if (value) {
 				// init, add new property
 				utils.addProperty(j, p, key, value, null);
