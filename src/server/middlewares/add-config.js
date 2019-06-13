df2c0017ccdcb11c9484240dfe8b528d07af369b
rishabh3112 <rishabh31121999@gmail.com>
2019-06-13 16:12:06 +0530

    migrate: create config middleware

---
+++ src/server/middlewares/add-config.js
@@ -0,0 +1,18 @@
+const { existsSync, readFileSync } = require('fs');
+const { resolve } = require('path');
+
+const WEBPACK_CONFIG_PATH = process.env.WEBPACK_CONFIG_PATH ? process.env.WEBPACK_CONFIG_PATH : "./webpack.config.js";
+
+export function addConfig(req, res, next) {
+    const configPath = resolve(process.cwd(), WEBPACK_CONFIG_PATH);
+    const res_json = res.json;
+    res.json = function (body) {
+        if (existsSync(configPath)) {
+            body.webpack = readFileSync(configPath).toString();
+        }
+        try {
+            res_json.call(this, body);   
+        } catch (error) {}
+    }
+    next();
+}
