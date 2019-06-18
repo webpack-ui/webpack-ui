e95aa6c1893a8069b4e57e2137ca9f1d3db66089
rishabh3112 <rishabh31121999@gmail.com>
2019-06-18 13:50:32 +0530

    migrate: write init route

--- src/server/routers/scaffolder.js
+++ src/server/routers/scaffolder.js
@@ -16,3 +16,23 @@ router.post('/save', (req, res) => {
     writeFileSync(resolve(USER_DIRECTORY, WEBPACK_CONFIG_PATH), req.body.webpack);
     res.json( { status: '200' });
 })
+
+router.post('/init', async (req, res) => {
+    if (req.body.type === 'defaults') {
+        runAction('init', defaultGenerator).then(() => {
+            res.json({value: true});
+        });
+        return;
+    }
+
+    if (!existsSync(resolve(`${USER_DIRECTORY}/node_modules/${req.body.type}`))) {
+        await execSync(`cd ${USER_DIRECTORY} && npm install --save-dev ${req.body.type}`);
+    }
+    
+    const Generator = require(`${USER_DIRECTORY}/node_modules/${req.body.type}`);
+    Generator.prototype.templatePath = (file) => `${USER_DIRECTORY}/node_modules/${req.body.type}/templates/${file}`;
+    Generator.prototype.destinationPath = (file) => `${USER_DIRECTORY}/${file}`;
+
+    runAction('init', Generator, req.body.type, );
+    res.json({value: true});    
+})
