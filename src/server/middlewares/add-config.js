1d4c5e35f95e6bd28f36eb0d3fb4f18cd121a011
rishabh3112 <rishabh31121999@gmail.com>
2019-06-18 13:49:22 +0530

    migrate: write refesh route

--- src/server/middlewares/add-config.js
+++ src/server/middlewares/add-config.js
@@ -1,7 +1,7 @@
 import { existsSync, readFileSync } from 'fs';
 import { resolve } from 'path';
 
-const WEBPACK_CONFIG_PATH = process.env.WEBPACK_CONFIG_PATH ? process.env.WEBPACK_CONFIG_PATH : "./webpack.config.js";
+const WEBPACK_CONFIG_PATH = process.env.WEBPACK_CONFIG_PATH ? process.env.WEBPACK_CONFIG_PATH : "webpack.config.js";
 
 export function addConfig(req, res, next) {
     const configPath = resolve(process.cwd(), WEBPACK_CONFIG_PATH);
--- src/server/routers/scaffolder.js
+++ src/server/routers/scaffolder.js
@@ -1,1 +1,14 @@
 import { writeFileSync, existsSync } from "fs";
+import {execSync} from 'child_process';
+import {join, resolve} from 'path';
+import { Router } from "express"
+
+import defaultGenerator from "../../utils/generators/default";
+import runAction from "../../utils/run-action";
+
+const USER_DIRECTORY = process.env.PWD ? process.env.PWD : process.cwd();
+const WEBPACK_CONFIG_PATH = process.env.WEBPACK_CONFIG_PATH ? process.env.WEBPACK_CONFIG_PATH : "webpack.config.js";
+const router = Router();
+
+router.get('/refresh', (req, res) => { res.json({ status: '200' }) });
+
