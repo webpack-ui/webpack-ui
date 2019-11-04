b6639017f7c03a2e0acf3b47946b76b4411d92f2
Dave Marquess <dave.marquess@gmail.com>
2019-11-03 19:04:26 -0800

    change <a> to <button>, add getScaffold method onClick

--- src/client/components/search/HitBox.jsx
+++ src/client/components/search/HitBox.jsx
@@ -7,23 +7,37 @@ function htmlDecode(input) {
 }
 
 class HitBox extends Component {
+  constructor(props) {
+    super(props);
+    this.getScaffold = this.getScaffold.bind(this);
+  }
+
+  getScaffold(scaffoldUrl) {
+    console.log('scaffoldUrl: ', scaffoldUrl);
+  }
+
   render() {
     const { hit } = this.props;
     console.log('hit: ', hit);
+
+    const scaffoldUrl =
+      hit.repository && hit.repository.url
+        ? hit.repository.url
+        : `https://npmjs.com/package/${hit.name}`;
+
     return (
-      <article className='hit-card'>
+      <article className="hit-card">
         {hit && (
-          <a
-            target='_blank'
-            rel='noopener noreferrer'
-            href={hit.repository && hit.repository.url ? hit.repository.url : `https://npmjs.com/package/${hit.name}`}>
-            <div className={styles['product-wrapper']}>
+          <div className={styles['product-wrapper']}>
+            <button
+              onClick={() => this.getScaffold(scaffoldUrl)}
+              className={styles['scaffold-button']}>
               <h3 className={styles['product-title']}>{hit.name}</h3>
               {/* <img className={styles['product-title']} src={hit.owner.avatar} height='40' width='40' />*/}
               <h3 className={styles['product-name']}>{hit.owner.name}</h3>
               {/* <p className={styles['product-desc']}>{htmlDecode(hit.description)}</p>*/}
-            </div>
-          </a>
+            </button>
+          </div>
         )}
       </article>
     );
